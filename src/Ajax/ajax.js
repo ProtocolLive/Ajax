814425179dc403549ccf6fed22e3a119bae2e644
Protocol Corporation <39626142+ProtocolLive@users.noreply.github.com>
2020-07-29 07:51:32 -0300

    Updates and performance
    - Changed " to ';
    - Removed AjaxParseSend;
    - Removed Refreshers;
    - AjaxXhr sending form data via FormData;
    - Rename AjaxXtr;

--- src/Ajax/ajax.js
+++ src/Ajax/ajax.js
@@ -1,123 +1,95 @@
 // Protocol Corporation Ltda.
 // https://github.com/ProtocolLive/Ajax
-// Version 2020.07.29.00
+// Version 2020.07.29.01
 
-if(typeof AjaxObject === "undefined"){
-  var AjaxObject = [];
-  var AjaxRefreshers = [];
-  var AjaxLoading = '';
-}
-
-if(typeof AjaxParseSend == "undefined"){
-  function AjaxParseSend(Form){
-    let send = "";
-    Form = document.forms[Form].elements;
-    for(let i = 0; i < Form.length; i++){
-      if(Form[i].name != ""){
-        if(Form[i].type == "radio"){
-          if(Form[i].checked == true){
-            send += Form[i].name + "=" + Form[i].value;
-          }
-        }else{
-          send += Form[i].name + "=" + encodeURIComponent(Form[i].value);
-        }
-        if(i < (Form.length - 1)){
-          send += "&";
-        }
-      }
-    }
-    return send;
-  }
+if(typeof AjaxObject === 'undefined'){
+  var AjaxObject = []
+  var AjaxLoading = ''
 }
 
 function AjaxExecute(Place){
-  let Command;
-  let Text = document.getElementById(Place).innerHTML;
-  while(Text.indexOf("<script") >= 0){
-    Text = Text.substr(Text.indexOf("<script") + 7);
-    Text = Text.substr(Text.indexOf(">") + 1);
-    Command = Text.substr(0, Text.indexOf("<\/script>"));
-    if(Command != ""){
-      window.eval(Command);
+  let Command
+  let Text = document.getElementById(Place).innerHTML
+  while(Text.indexOf('<script') >= 0){
+    Text = Text.substr(Text.indexOf('<script') + 7)
+    Text = Text.substr(Text.indexOf('>') + 1)
+    Command = Text.substr(0, Text.indexOf('<\/script>'))
+    if(Command !== ''){
+      window.eval(Command)
     }
-    Text = Text.substr(Text.indexOf("<\/script>") + 9);
+    Text = Text.substr(Text.indexOf('<\/script>') + 9)
   }
 }
 
-function AjaxFetch(Url, Return, Form, Refresh){
-  let Data
+function AjaxFetch(Url, Return, Form){
+  let Data = null
   if(Form !== undefined){
     Data = {
       method: 'POST',
       body: new FormData(document.forms[Form])
     }
-  }else{
-    Data = null
   }
-  fetch(Url, Data)
-  .then(document.getElementById(Return).innerHTML = AjaxLoading)
-  .then((response)=>{
-    response.text()
-    .then((result)=>{
-      document.getElementById(Return).innerHTML = result
-    })
-  })
+  fetch(Url, Data).then(
+    document.getElementById(Return).innerHTML = AjaxLoading
+  ).then(
+    (response)=>{
+      response.text().then(
+        (result)=>{
+          document.getElementById(Return).innerHTML = result
+          AjaxExecute(Return)
+        }
+      )
+    }
+  )
 }
 
-function AjaxXTR(Url, Return, Form, Refresh){
-  let Data = null;
+function AjaxXtr(Url, Return, Form){
+  const place = document.getElementById(Return).innerHTML
+  let Data = null
   if(AjaxObject[Return] == undefined){
     try{
-      AjaxObject[Return] = new XMLHttpRequest();
+      AjaxObject[Return] = new XMLHttpRequest()
     }catch(e){
       try{
-        AjaxObject[Return] = new ActiveXObject("Microsoft.XMLHTTP");
+        AjaxObject[Return] = new ActiveXObject('Microsoft.XMLHTTP')
       }catch(e){
-        AjaxObject[Return] = new ActiveXObject("Msxml2.XMLHTTP");
+        AjaxObject[Return] = new ActiveXObject('Msxml2.XMLHTTP')
       }
     }
   }
-  AjaxObject[Return].onreadystatechange = function(){
+  AjaxObject[Return].onreadystatechange = ()=>{
     if(AjaxObject[Return].readyState == 1){
-      document.getElementById(Return).innerHTML = AjaxLoading;
-      document.documentElement.style.cursor = "progress";
+      place = AjaxLoading
+      document.documentElement.style.cursor = 'progress'
     }else if(AjaxObject[Return].readyState == 3){
-      document.getElementById(Return).innerHTML = AjaxLoading;
-      document.getElementById(Return).innerHTML += AjaxObject[Return].responseText;
+      place = AjaxLoading += AjaxObject[Return].responseText
     }else if(AjaxObject[Return].readyState == 4 && AjaxObject[Return].status == 404){
-      document.getElementById(Return).innerHTML = "Error 404: Page not found";
-      document.documentElement.style.cursor = "default";
+      place = 'Error 404: Page not found'
+      document.documentElement.style.cursor = 'default'
     }else if(AjaxObject[Return].readyState == 4){
-      document.getElementById(Return).innerHTML = AjaxObject[Return].responseText;
-      if(Refresh !== undefined){
-        AjaxRefreshers[Return] = setTimeout(function(){
-          Ajax(Url, Return, null, Refresh);
-        }, Refresh);
-      }
-      AjaxExecute(Return);
-      document.documentElement.style.cursor = "default";
+      place = AjaxObject[Return].responseText
+      AjaxExecute(Return)
+      document.documentElement.style.cursor = 'default'
     }
   }
-  AjaxObject[Return].ontimeout = function(e) {
-    document.getElementById(Return).innerHTML = "Loading timeout!";
-    document.documentElement.style.cursor = "default";
-  };
-  if(typeof Form === "undefined" || Form == null){
-    AjaxObject[Return].open("GET", Url, true);
-  }else{
-    Data = AjaxParseSend(Form);
-    AjaxObject[Return].open("POST", Url, true);
-    AjaxObject[Return].setRequestHeader("Content-type", "application/x-www-form-Urlencoded");
+  AjaxObject[Return].ontimeout = (e)=>{
+    place = 'Loading timeout!'
+    document.documentElement.style.cursor = 'default'
   }
-  AjaxObject[Return].timeout = 60000;
-  AjaxObject[Return].send(Data);
+  if(typeof Form === 'undefined' || Form === null){
+    AjaxObject[Return].open('GET', Url, true)
+  }else{
+    Data = new FormData(document.forms[Form])
+    AjaxObject[Return].open('POST', Url, true)
+  }
+  AjaxObject[Return].timeout = 60000
+  AjaxObject[Return].send(Data)
 }
 
-function Ajax(Url, Return, Form, Refresh){
-  clearTimeout(AjaxRefreshers[Return]);
+function Ajax(Url, Return, Form){
   if('fetch' in window){
-    AjaxFetch(Url, Return, Form, Refresh)
+    AjaxFetch(Url, Return, Form)
   }else{
-    AjaxXTR(Url, Return, Form, Refresh)
+    AjaxXtr(Url, Return, Form)
   }
 }
